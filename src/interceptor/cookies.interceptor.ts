3b6b01faa92068c7c161da1263493a63ce963bfb
felcarvalho <felcarvalho54321@gmail.com>
2026-08-31 22:01:10 -0300

    feat(interceptor): adicionar CookiesTokensInterceptor
    - Intercepta resposta de login/refresh para setar cookie sessionId httpOnly

---
+++ src/interceptor/cookies.interceptor.ts
@@ -0,0 +1,26 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { map, Observable } from 'rxjs';
+import { Response } from 'express';
+
+@Injectable()
+export class CookiesTokensInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((value) => {
+        const res = context.switchToHttp().getResponse<Response>();
+        res.cookie('sessionId', value, {
+          httpOnly: true,
+          sameSite: 'lax',
+          path: '/',
+        });
+
+        return;
+      }),
+    );
+  }
+}
