7b144e50b66ba5eaf0570dcb75ef083f72ef7de6
felcarvalho <felcarvalho54321@gmail.com>
2026-09-08 02:39:36 -0300

    refactor(create-user): remover import não utilizado do UserModule

--- src/shared/orchestrators/create-user/create-user.module.ts
+++ src/shared/orchestrators/create-user/create-user.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { CreateUserService } from './create-user.service';
 import { AuthenticationModule } from '../../../authentication/authentication.module';
-import { UserModule } from '../../../user/user.module';
 import { ModuleCore } from '../../../shared/moduleCore/module.core';
 import { CreateUserController } from './create-user.controller';
 
