516e72b8e7e978b11132b76e5646604d7d9e47e0
felcarvalho <felcarvalho54321@gmail.com>
2026-09-13 03:30:54 -0300

    refactor: configurar CORS origens via variáveis de ambiente

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:4200'],
+    origin: [process.env.ORIGIN_URL_LOCAL, process.env.ORIGIN_URL_PROD],
     credentials: true,
   });
 
