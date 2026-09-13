f7e69787c9d3973a8ab97cb8ab2b7d18603910c8
felcarvalho <felcarvalho54321@gmail.com>
2026-09-13 06:51:58 -0300

    fix: listen on all interfaces (0.0.0.0)

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,7 @@ async function bootstrap() {
     jsonDocumentUrl: 'swagger/json',
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
 
